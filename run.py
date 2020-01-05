b063f324a0f6040a5f575b5b94e12ef749e2fdda
TheTraveller95 <crivellaroandres@gmail.com>
2020-01-05 11:36:07 +0000

    add chatbox function and clean the code

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
-def add_messages(username, message):
+def add_message(username, message):
     """Add messages to the 'messages' list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp":now , "from": username , "message": message}
@@ -23,20 +23,26 @@ def index():
         session["username"]=request.form["username"]
 
     if "username" in session:
-        return redirect(session["username"])
+        return redirect(url_for("user", username = session["username"]))
 
     return render_template('index.html')
 
-@app.route('/<username>') #this will return Hi username every time we put a username inside the url preceded by /
+@app.route('/chat/<username>', methods=["GET","POST"]) 
 def user(username):
     """Display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username = session["username"])) # here we use the redirect instead of render_ttemplate because otherwise the message we wrote will be added every 5 sec in an infinite loop
+
     return render_template ('chat.html', username = username , chat_messages = messages)
 
-@app.route('/<username>/<message>') #this will return username: message every time we put a username inside the url preceded by / and then /+message
+'''@app.route('/<username>/<message>') #this will return username: message every time we put a username inside the url preceded by / and then /+message
 def send_message(username, message):
     """Create a message and  redirect cack to the chat page"""
-    add_messages(username, message)
-    return redirect('/' + username)
+    add_message(username, message)
+    return redirect('/' + username)'''
 
 
 
